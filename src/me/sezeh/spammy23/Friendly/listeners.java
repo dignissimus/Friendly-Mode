378d4096a64963f1b76791499f8023507bb48142
Sam Ezeh <spammy23@users.noreply.github.com>
2016-10-28 01:06:57 +0100

    Add files via upload

--- src/me/sezeh/spammy23/Friendly/listeners.java
+++ src/me/sezeh/spammy23/Friendly/listeners.java
@@ -1,11 +1,15 @@
 package me.sezeh.spammy23.Friendly;
 
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.awt.*;
@@ -18,27 +22,31 @@ import static me.sezeh.spammy23.Friendly.Main.pluginthis;
  */
 public class listeners implements Listener {
     @EventHandler
-    public void onAttack(EntityDamageByEntityEvent event){
+    public void onAttack(EntityDamageByEntityEvent event) {
         Player attacked;
         Player attacker;
         Double damage;
-        if(event.getEntity() instanceof Player && event.getDamager() instanceof Player) {
-            /**
-            attacked=(Player)event.getEntity();
-            if (pluginthis.getConfig().getBoolean(attacked.getName())) {
-                damage = event.getDamage();
-                attacker = (Player) event.getDamager();
-                attacked.setHealth(attacked.getHealth() + damage);//Heals
+        if (event.getEntity() instanceof Player && (event.getDamager() instanceof Player || event.getDamager() instanceof Arrow)) {
 
-                attacker.sendMessage(ChatColor.RED + attacked.getName() + ChatColor.WHITE + " Has Friendly Mode Turned OFF");
+            attacked = (Player) event.getEntity();
+            //System.out.println(attacked.getName()+" was hurt");
+            if (pluginthis.getConfig().getBoolean(attacked.getName())) {
+                //System.out.println("cancel");
+                damage = event.getDamage();
+                //attacked.setHealth(attacked.getHealth() + damage);//Heals
+                if (event.getDamager() instanceof Player) {
+                    attacker = (Player) event.getDamager();
+                    attacker.sendMessage(ChatColor.RED + attacked.getName() + ChatColor.WHITE + " Has Friendly Mode Turned OFF");
+                }
+                event.setCancelled(true);
             }
-             **/
-            event.setCancelled(true);
         }
     }
+
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event){
         Player player = (Player) event.getPlayer();
+     //   System.out.println(player.getName()+" joined");
         if (!pluginthis.getConfig().contains(player.getName())){
             pluginthis.getConfig().set(player.getName(), false);
             try {
@@ -49,5 +57,22 @@ public class listeners implements Listener {
             }
         }
     }
-}
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event){
+        if (event.getEntity() instanceof Player) {
+            Player killed=(Player) event.getEntity();
+      //      System.out.println(killed.getName()+" Died");
+            //System.out.println(pluginthis.getConfig().getBoolean(killed.getName()));
+            if (pluginthis.getConfig().getBoolean(killed.getName())){
+                event.setKeepInventory(true);
+
+                }
+            else {
+                event.setKeepInventory(false);
+
+            }
+
+            }
+        }
+    }
 
